c863396b37fa6f00fef45bdde601a6c39f460a8f
Kyndt Clovis <clovis@1000mercis.com>
2017-07-20 12:30:53 +0200

    enhance print + add args

--- cityhash/python/main.py
+++ cityhash/python/main.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
+
 from cityhash import CityHash128 as ch
 
 # open file and hash each line
-def hash_from_file(filename, constraint):
+def hash_from_file(filename, constraint, max_read):
     hashs = []
-    with open(filename, 'r') as f:
-        for line in f:
-            if (constraint):
-                hashs.append(ch(line) % constraint)
-            else:
-                hashs.append(ch(line))
-    return hashs
+    try:
+        f = open(filename, 'r')
+    except IOError:
+        print("Error: file not found")
+        return hashs, True
+    else:
+        with f:
+            count = 0
+            for line in f:
+                if max_read != 0 and count >= max_read:
+                    print("- Stop to read file after {:,} lines".format(count))
+                    break;
+                if (constraint):
+                    hashs.append(ch(line) % constraint)
+                else:
+                    hashs.append(ch(line))
+                count += 1
+    return hashs, False
 
 def find_duplication(hashs):
     duplicat = 0
@@ -25,13 +38,34 @@ def find_duplication(hashs):
     return duplicat
 
 def print_result(hashs, duplicat):
-    print("Hash: {:,}   ||   Duplicate: {:,}   ||   {}%".format(len(hashs), duplicat, duplicat / len(hashs) * 100))
+    print("= Hash: {:,}\n= Duplicate: {:,}\n= Rate: {:.2f}%".format(len(hashs), duplicat, duplicat / len(hashs) * 100))
 
-def main():
-    hashs = hash_from_file("read.txt", 62 ** 6)
+def main(parser):
+    args = parser.parse_args()
+    print('- Applied constraint {:,}'.format(args.constraint))
+    hashs, err = hash_from_file(args.file[0], args.constraint, args.max)
+    if len(hashs) == 0:
+        if err is False:
+            print("Error: no value found from the file")
+        return
     hashs.sort()
     duplicat = find_duplication(hashs)
     print_result(hashs, duplicat)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file",
+                        nargs=1,
+                        type=str,
+                        help="Dest file of urls")
+    parser.add_argument("-c",
+                        "--constraint",
+                        type=int,
+                        default=62 ** 6,
+                        help="Constraint force hash to be contained in a range")
+    parser.add_argument("-m",
+                        "--max",
+                        type=int,
+                        default=0,
+                        help="Max read of line")
+    main(parser)
